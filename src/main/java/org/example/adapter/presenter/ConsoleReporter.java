479fc70d7d0f279dbe2a3bcadc79fa61703c3e38
David Abramovich <david.abramovich84@gmail.com>
2023-02-08 00:20:00 -0300

    refactor ConsoleReporter

--- src/main/java/org/example/adapter/presenter/ConsoleReporter.java
+++ src/main/java/org/example/adapter/presenter/ConsoleReporter.java
@@ -5,11 +5,18 @@ import org.example.core.usecase.port.dto.DoorDto;
 import org.example.core.usecase.port.dto.OutputDto;
 
 public class ConsoleReporter implements Reporter {
-    
+
     @Override
     public void result(OutputDto outputDto) {
-        System.out.println("door_number state");
+        printHeaders();
+        printContentOf(outputDto);
+    }
 
+    private void printHeaders() {
+        System.out.println("door_number state");
+    }
+
+    private void printContentOf(OutputDto outputDto) {
         for(DoorDto door : outputDto.getDoors()) {
             System.out.println(door.getNumber() + " " + door.getState());
         }
